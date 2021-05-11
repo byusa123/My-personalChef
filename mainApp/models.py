71a1043b96fe465693e4da4850ed00b219e5581a
IngabireTina <ingabire067@gmail.com>
2021-05-11 15:13:34 -0400

    added meal class

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from account.models import *
+from personalChef import settings
+
 
 # Create your models here.
+
+CATEGORY = [
+    ('Lunch', 'Lunch'),
+    ('BreakFast', 'BreakFast'),
+    ('Dinner', 'Dinner'),
+]
+
+# MEAL CLASS
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=30)
+    date_created = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=40, choices=CATEGORY, default='Dinner')
+    
+    def __str__(self):
+        return self.name
+
+
+
+
