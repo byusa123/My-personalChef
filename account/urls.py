08c798085c348a91f6c0354bf9832e6625f42051
IngabireTina <ingabire067@gmail.com>
2021-05-13 05:07:07 -0400

    added homepage and adimin panel

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='homePage'),
+    path('home/', views.homePage1, name='homePage1'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
-# from .models importUser, 
+from .models import User
+
 # Create your views here.
 
 def homePage(request):
     # users = A.objects.all()
     # context = {'users': users}
+    return render(request, 'admin/base.html')
+
+
+def homePage1(request):
+    # users = A.objects.all()
+    # context = {'users': users}
     return render(request, 'index.html')
+
+
+
+def registerPage(request):
+    context = {}
+    return render()
