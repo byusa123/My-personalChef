c0807e2ceb498398235145e8be49eeca677f4078
Charity Mutoni <charitytoni0@gmail.com>
2021-05-14 18:25:36 +0200

    creating api

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
-urlpatterns = [
-    # path('', views.homePage, name='homePage'),
-]
+urlpatterns=[
+    # url('^$',views.index,name = 'index'),
+    url(r'^api/meals/$', views.MealList.as_view()),
+    url(r'^api/shedule/$', views.ScheduleList.as_view()),
+    url(r'^api/booking/$', views.bookingList.as_view()),
+] 
--- personalChef/settings.py
+++ personalChef/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'account',
     'mainApp',
     'crispy_forms',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
